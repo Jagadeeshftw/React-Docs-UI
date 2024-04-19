f68bbda3cae5ef0bf2155fb2ef2dd600a691ba80
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-04-20 01:20:17 +0530

    Split product in different Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/headers";
-import Concepts from "./components/concepts";
+
+const Concepts = ({ image, title, description }) => {
+  return (
+    <li>
+      <img src={image}></img>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+};
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <section>
-          {CORE_CONCEPTS.map((item)=> <Concepts />)}
+        <section id='core-concepts'>
+        <h2>Core Concepts</h2>
+        <ul>
+        {CORE_CONCEPTS.map((item)=> <Concepts {...item} />)}
+        </ul>
         </section>
       </main>
     </div>
---
+++ src/components/coreConcepts.jsx
@@ -0,0 +1,11 @@
+const Concepts = ({ image, title, description }) => {
+  return (
+    <li>
+      <img src={image}></img>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+};
+
+export default Concepts;
