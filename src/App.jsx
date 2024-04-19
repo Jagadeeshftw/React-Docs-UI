bb1bd6669659d51703eeffd047d96388ea641cf2
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-04-20 00:55:53 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
+import Header from "./components/headers";
+import Concepts from "./components/concepts";
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section>
+          {CORE_CONCEPTS.map((item)=> <Concepts />)}
+        </section>
       </main>
     </div>
   );
